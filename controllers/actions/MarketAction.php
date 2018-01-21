d21ff8b1366bbd44fa95a960326ba7d622d71d6d
Oceatoon <oceatoon@gmail.com>
2018-01-21 14:27:24 +0400

    market action for classifieds

--- controllers/actions/MarketAction.php
+++ controllers/actions/MarketAction.php
@@ -3,7 +3,7 @@ class MarketAction extends CAction
 {
     public function run()
     {
-    	echo $this->getController()->renderPartial("search",array("type"=>"classified"));
+    	echo $this->getController()->renderPartial("co2.views.app.search",array("type"=>"classified"));
     }
 }
 
