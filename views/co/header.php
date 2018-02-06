a92ed88dbcb06408e0924f4b0e24d79625334f61
Oceatoon <oceatoon@gmail.com>
2018-02-06 16:56:17 +0400

    pulling classified app stuff directory to module

---
+++ views/co/header.php
@@ -0,0 +1,19 @@
+<?php 
+
+$classified = CO2::getModuleContextList("classifieds","categories");
+$currentSection = 1;
+
+foreach ($classified["sections"] as $key => $section) 
+{ ?>
+
+  <div class="col-md-2 col-sm-4 col-xs-6 no-padding">
+    <button class="btn btn-default col-md-12 col-sm-12 padding-10 bold text-dark elipsis btn-select-type-anc btn-select-filliaire" 
+            data-type-anc="<?php echo @$section["label"]; ?>" data-key="<?php echo @$section["key"]; ?>" 
+            data-type="classified"
+            style="border-radius:0px; border-color: transparent; text-transform: uppercase;">
+      <i class="fa fa-<?php echo $section["icon"]; ?> fa-2x hidden-xs"></i><br><?php echo Yii::t("category",$section["label"]); ?>
+    </button>
+  </div>
+
+<?php 
+} ?>
